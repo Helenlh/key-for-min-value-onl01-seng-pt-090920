08c6bba42d1ded26b68ee632f455ebccb1623598
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-28 03:05:00 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,33 +2,48 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-describe "smallest hash value" do
+#describe "smallest hash value" do
 
 def smallest_hash_value(hash) 
   hash = {:blake => 500, :ashley => 2, :adam => 1}
-  key_for_min_value.min(1)
+  key_for_min_value.min(1).key 
 end 
 
-  it "does not call the `#keys` method" do
+  #it "does not call the `#keys` method" do
     hash = {:blake => 500, :ashley => 2, :adam => 1}
+    hash.each do (name, number)
+    together << number
+    together.to_a 
+   # expect(hash).to_not receive(:keys)
+end 
+  together 
+    
 
-    expect(hash).to_not receive(:keys)
+  #it "does not call the `#values` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+    hash.each do (name, number)
+    together << name
+    together.to_a 
+   #
+
+
+    #expect(hash).to_not receive(:values)
+
 
     key_for_min_value(hash)
   end
 
-  it "does not call the `#values` method" do
+ # it "does not call the `#min method" do
     hash = {:blake => 500, :ashley => 2, :adam => 1}
 
-    expect(hash).to_not receive(:values)
+   # expect(hash).to_not receive(:min)
 
     key_for_min_value(hash)
-  end
+    hash.each do {|a,b| a<=> b}
+  end 
+end 
 
-  it "does not call the `#min method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:min)
-
-    key_for_min_value(hash
+  
 end
+end 
