e8a6b914c89f7a6e25c8de1352184b0122825838
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-05 01:53:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,49 +1,41 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-#describe "smallest hash value" do
+
 
 def smallest_hash_value(hash) 
   hash = {:blake => 500, :ashley => 2, :adam => 1}
   key_for_min_value.min(1).key 
 end 
 
-  #it "does not call the `#keys` method" do
+ 
     hash = {:blake => 500, :ashley => 2, :adam => 1}
     hash.each do (name, number)
+    hash.min 
     together << number
     together.to_a 
-   # expect(hash).to_not receive(:keys)
 end 
   together 
+  
+  
     
 
   #it "does not call the `#values` method" do
     hash = {:blake => 500, :ashley => 2, :adam => 1}
     hash = {:blake => 500, :ashley => 2, :adam => 1}
-    hash.each do (name, number)
+    hash.each (name, number)
+    hash.min 
     together << name
     together.to_a 
-   #
-
-
-    #expect(hash).to_not receive(:values)
-
-
-    key_for_min_value(hash)
   end
+
+
 
  # it "does not call the `#min method" do
     hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-   # expect(hash).to_not receive(:min)
-
-    key_for_min_value(hash)
-    hash.each do {|a,b| a<=> b}
-  end 
-end 
-
-  
+def min_key_value_pair (test)
+  if test == {}
+       return nil
+  else
+       test = test.sort_by {|k, v| v}
+       test[0]
+   end
 end
-end 
